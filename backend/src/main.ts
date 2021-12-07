f122c02c51eb6e118862c310743f29be90e889c4
Dewa Ade-onojobi <adewalade@gmail.com>
2021-12-07 19:54:38 +0100

    feat: add test jobs to circleci config file
    Jobs added:
    - Test Frontend
    - Test Backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('customTag')
     .setBasePath(apiVersionPrefix)
-    .addBearerAuth()
+    .addBearerAuth() // Error 01 has been fixed
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
